f39b7f320b64aab136721db1c9076376960e5d54
cchampou <clement@champouillon.com>
2020-01-09 04:19:38 +0100

    With users search

--- src/resources/lists/users.jsx
+++ src/resources/lists/users.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import {
-  List, Datagrid, EmailField, TextField, ReferenceField,
+  List, Datagrid, EmailField, TextField, ReferenceField, Filter, TextInput,
 } from 'react-admin';
 
+const UserFilter = (props) => (
+  <Filter {...props}>
+    <TextInput label="Prénom" source="firstname" />
+    <TextInput label="Nom" source="name" />
+    <TextInput label="Email" source="email" />
+    <TextInput label="Téléphone" source="phone" />
+  </Filter>
+);
+
 export default (props) => (
-  <List {...props}>
+  <List filters={<UserFilter />} {...props}>
     <Datagrid rowClick="show">
       <TextField source="firstname" />
       <TextField source="name" />
